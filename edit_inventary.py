fe6ad00d3d5bb787f81189f6242d5a2a20c332f9
uwo-o <hugo.campos@usm.cl>
2021-06-18 23:08:27 -0400

    Its ready, i guess

--- edit_inventary.py
+++ edit_inventary.py
@@ -37,7 +37,7 @@ def delete_product(config,table,id_entry):
     conection=sqlite3.connect(config["DATA_BASE_NAME"])
     cursor=conection.cursor()
     try:
-        if id_entry.get!="":
+        if id_entry.get()!="":
             cursor.execute("DELETE FROM Inventario WHERE ID='{}'".format(id_entry.get()))
             conection.commit()
             update_table(config,table)
@@ -45,6 +45,22 @@ def delete_product(config,table,id_entry):
         else:
             messagebox.showinfo("ADVERTENCIA","LOS CAMPOS NO PUEDEN ESTAR VACÍOS")
 
+    except:
+        pass
+
+def update_stock(config,table,id_entry2,quantity_entry2):
+    conection=sqlite3.connect(config["DATA_BASE_NAME"])
+    cursor=conection.cursor()
+    try:
+        if id_entry2.get()!="" and quantity_entry2.get()!="":
+            cursor.execute("UPDATE Inventario SET CANTIDAD={} WHERE ID = '{}'".format(quantity_entry2.get(),id_entry2.get()))
+            conection.commit()
+            update_table(config,table)
+            id_entry2.delete(0,"end")
+            quantity_entry2.delete(0,"end")
+        else:
+            messagebox.showinfo("ADVERTENCIA","LOS CAMPOS NO PUEDEN ESTAR VACÍOS")
+    
     except:
         pass
 
@@ -58,7 +74,8 @@ def open_inventary(config):
     product=StringVar()
     quantity=StringVar()
     id=StringVar()
-
+    id2=StringVar()
+    quantity2=StringVar()
     Label(root,text="PRODUCTO").grid(padx=10,row=0, column=0)
     Label(root,text="CANTIDAD").grid(row=0, column=2)
 
@@ -87,6 +104,16 @@ def open_inventary(config):
     id_entry.place(x=30,y=455)
 
     Button(root,text="ACEPTAR",command=lambda:delete_product(config,table,id_entry)).place(x=200,y=450)
+
+    Label(root,text="ACTUALIZAR STOCK").place(x=140,y=480)
+    Label(root,text="ID").place(x=5,y=505)
+    id_entry2=Entry(root,width=25,textvariable=id2)
+    id_entry2.place(x=30,y=505)
+    Label(root,text="CANTIDAD").place(x=5,y=535)
+    quantity_entry2=Entry(root,width=18,textvariable=quantity2)
+    quantity_entry2.place(x=70,y=535)
+
+    Button(root,text="ACEPTAR",command=lambda:update_stock(config,table,id_entry2,quantity_entry2)).place(x=200,y=520)
     
 
     update_table(config,table)
